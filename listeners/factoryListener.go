a8a11f6c3778a6ebb7392f163474491c50c656bc
hsouf <soufianehajazi23@gmail.com>
2022-10-29 00:04:18 +0100

    resubscribe to event when socket disconnects

--- listeners/factoryListener.go
+++ listeners/factoryListener.go
@@ -4,16 +4,17 @@ import (
 	"context"
 	"fmt"
 
-	"main/walletFactory"
-
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/ethereum/go-ethereum/event"
+	log "github.com/sirupsen/logrus"
+	"main/walletFactory"
+	"time"
 )
 
-func FactoryListener(factoryAddress string, endpoint string) {
+func FactoryListener(factoryAddress string, endpoint string) { //connect to local node
 
-	//connect to local node
 	cl, err := ethclient.Dial("ws://127.0.0.1:9999")
 
 	ctx := context.Background()
@@ -24,6 +25,7 @@ func FactoryListener(factoryAddress string, endpoint string) {
 		panic(err)
 	}
 
+	////////////////////////////////////////////////////////////////////////////////////////
 	factory, err := walletFactory.NewWalletFactory(addr, cl)
 	callOpts := &bind.CallOpts{Context: ctx, Pending: false}
 	cost, err := factory.MintCost(callOpts)
@@ -32,35 +34,43 @@ func FactoryListener(factoryAddress string, endpoint string) {
 	// Watch for a minted wallets events on Factory
 	watchOpts := &bind.WatchOpts{Context: ctx, Start: nil}
 
-	// Setup a channel for minted wallets
+	fmt.Println("connecting to socket.....")
+	backoffMax := 100 * time.Millisecond //generous backoff ^^
 	walletsChannel := make(chan *walletFactory.WalletFactoryWalletMinted)
 
-	//var wg sync.WaitGroup
-	var socketConnected = false
-	for {
+	sub, err := factory.WatchWalletMinted(watchOpts, walletsChannel)
 
-		//connect to socket
-		if socketConnected == false {
-			sub, err := factory.WatchWalletMinted(watchOpts, walletsChannel)
-			if err != nil {
-				panic(err)
-
-			}
-			fmt.Println("connection established, listenning for mint events on", factoryAddress)
-			defer func() {
-				sub.Unsubscribe()
-				socketConnected = false
-
-			}()
-
-		}
-		socketConnected = true
-
-		event := <-walletsChannel
-		go addWalletToDb(event)
-
+	if err != nil {
+		//first connection needs to succeed
+		panic(err)
 	}
 
+	log.WithFields(log.Fields{
+		"factory": factoryAddress,
+	}).Info("connection established, listenning for mint events on")
+
+	defer func() {
+		sub.Unsubscribe()
+
+	}()
+	for {
+		select {
+		case <-sub.Err():
+
+			log.WithFields(log.Fields{
+				"timestamp": time.Now(),
+				"websocket": endpoint,
+			}).Warn("Socket disconnected, trying to reconnect...")
+
+			sub = event.Resubscribe(backoffMax, func(ctx context.Context) (event.Subscription, error) {
+				return factory.WatchWalletMinted(watchOpts, walletsChannel)
+			})
+
+		case event := <-walletsChannel:
+
+			go addWalletToDb(event)
+		}
+	}
 }
 
 func addWalletToDb(ch *walletFactory.WalletFactoryWalletMinted) {
